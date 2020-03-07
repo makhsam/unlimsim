868bf8539ff887ccb7ea6e31cde19ec9fba3d9d6
Sayidazim <sayidazimahmudov@gmail.com>
2020-03-07 14:26:03 +0500

    Login page & small changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	IMask(document.getElementById('phoneNumber'), {
-		mask: '+{7}(000)000-00-00',
+		mask: '+{7} (000) 000-00-00',
 		lazy: false,  // make placeholder always visible
 		placeholderChar: '_'     // defaults to '_'
 	}).on('accept', function() {
